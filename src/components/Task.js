032666b6fe7204200530049c07c1948104722762
Kira Sidhu <kira.kaur.sidhu@gmail.com>
2023-06-16 14:07:06 -0700

    Completed Wave 2

--- src/components/Task.js
+++ src/components/Task.js
@@ -4,18 +4,24 @@ import PropTypes from 'prop-types';
 import './Task.css';
 
 const Task = (props) => {
-  const [complete, setComplete] = useState(props.isComplete);
-  const buttonClass = complete ? 'tasks__item__toggle--completed' : '';
+  // const [complete, setComplete] = useState(props.isComplete);
+  const buttonClass = () => {
+    if (props.isComplete) {
+      return 'tasks__item__toggle--completed';
+    } else {
+      return '';
+    }
+  };
 
   return (
     <li className="tasks__item">
       <button
-        className={`tasks__item__toggle ${buttonClass}`}
-        onClick={() => setComplete(!complete)}
+        className={`tasks__item__toggle ${buttonClass()}`}
+        onClick={() => {props.changeComplete(props.id);}}
       >
         {props.title}
       </button>
-      <button className="tasks__item__remove button">x</button>
+      <button className="tasks__item__remove button" onClick={() => {props.deleteTask(props.id);}}>x</button>
     </li>
   );
 };
@@ -24,6 +30,8 @@ Task.propTypes = {
   id: PropTypes.number.isRequired,
   title: PropTypes.string.isRequired,
   isComplete: PropTypes.bool.isRequired,
+  changeComplete: PropTypes.func.isRequired,
+  deleteTask: PropTypes.func.isRequired,
 };
 
 export default Task;
