d2ad23099dada937467e4ea31e37f0913b52e0e1
Kira Sidhu <kira.kaur.sidhu@gmail.com>
2023-06-16 15:03:56 -0700

    Edited Wave 2

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,22 +1,16 @@
-import React, { useState } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
 import './Task.css';
 
 const Task = (props) => {
   // const [complete, setComplete] = useState(props.isComplete);
-  const buttonClass = () => {
-    if (props.isComplete) {
-      return 'tasks__item__toggle--completed';
-    } else {
-      return '';
-    }
-  };
+  const buttonClass = props.isComplete ? 'tasks__item__toggle--completed' : '';
 
   return (
     <li className="tasks__item">
       <button
-        className={`tasks__item__toggle ${buttonClass()}`}
+        className={`tasks__item__toggle ${buttonClass}`}
         onClick={() => {props.changeComplete(props.id);}}
       >
         {props.title}
